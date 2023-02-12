33fbb9987486a9329e1380f94458369be14da905
TRMmember <gfakemail18@gmail.com>
2023-02-12 19:25:37 +0500

    add a lil bit of logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Game from "./components/Game"
 const App = () => {
   return (
     <div className="bg-veryDark min-h-screen w-full flex flex-col items-center">
-      <h1 className="text-white text-5xl mt-10">The Simon Game</h1>
+      <h1 className="text-white text-5xl text-center mt-10">The Simon Game</h1>
 
       <Game />
+
+      <div className="mt-16"></div>
     </div>
   )
 }
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Game = () => {
 
@@ -8,24 +8,42 @@ const Game = () => {
         playerTurn: boolean;
         moves: string[];
         score: number;
+        turnCount: number;
     }
 
+    const [game, setGame] = useState<game>({
+        playerTurn: false,
+        moves: [],
+        score: 0,
+        turnCount: 0,
+    });
+
+    const startGame = () => {
+        setGame({...game, playerTurn: true});
+        
+    };
+
   return (
-    <div className='mt-16 flex flex-col items-center'>
+    <div className='mt-12 flex flex-col items-center'>
         
-        <div className="flex min-w-sm flex-col columns-2 gap-5">
-
-            <h2 className='text-white text-3xl self-center mb-5'>Your score is: </h2>
-
-            <div className="flex flex-row gap-5">
-                <div id='green' className="bg-greenCard w-32 h-32 cursor-pointer active:opacity-50"></div>
-                <div id='red' className="bg-redCard w-32 h-32 cursor-pointer active:opacity-50"></div>
+        <div className="flex min-w-sm flex-col columns-2 gap-12">
+            {/* Score Header */}
+            <h2 className='text-white text-3xl self-center '>Your score is: </h2>
+            {/* Game Plate */}
+            <div className="flex flex-col gap-5">
+                <div className="flex flex-row gap-5">
+                    <div id='green' className="bg-greenCard w-32 h-32 cursor-pointer active:opacity-50"></div>
+                    <div id='red' className="bg-redCard w-32 h-32 cursor-pointer active:opacity-50"></div>
+                </div>
+                <div className="flex flex-row gap-5">
+                    <div id='yellow' className="bg-yellowCard w-32 h-32 cursor-pointer active:opacity-50"></div>
+                    <div id='blue' className="bg-blueCard w-32 h-32 cursor-pointer active:opacity-50"></div>
+                </div>
             </div>
-            <div className="flex flex-row gap-5">
-                <div id='yellow' className="bg-yellowCard w-32 h-32 cursor-pointer active:opacity-50"></div>
-                <div id='blue' className="bg-blueCard w-32 h-32 cursor-pointer active:opacity-50"></div>
-            </div>
-        
+            {/* Start Button */}
+            <button className='bg-cardDark w-[70%] self-center text-white text-3xl tracking-widest py-3 rounded-[10px] active:bg-white active:text-cardDark' onClick={startGame}>
+                {game.playerTurn ? "STOP" : "START"}
+            </button>
         </div>
 
     </div>
