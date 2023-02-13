eae2854c26fb66570c8f34169abe89eb9839a5db
TRMmember <gfakemail18@gmail.com>
2023-02-13 15:43:58 +0500

    added some logic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,26 +2,50 @@ import React, { useState } from 'react'
 
 const Game = () => {
 
-    const color:string[] = ["green","red","yellow","blue"];
+    const colors:string[] = ["green","red","yellow","blue"];
 
     type game = {
         playerTurn: boolean;
-        moves: string[];
         score: number;
-        turnCount: number;
     }
 
     const [game, setGame] = useState<game>({
         playerTurn: false,
-        moves: [],
         score: 0,
-        turnCount: 0,
     });
 
     const startGame = () => {
+        let moves:string[] = [];
+        let turnCount = 1;
         setGame({...game, playerTurn: true});
+
+        const fillMoves = (moves:string[], turnCount:number) => {
+            while(moves.length < turnCount) {
+                console.log(moves.length);
+                console.log(turnCount);
+                moves.push(colors[Math.floor(Math.random()*colors.length)])
+                console.log(moves.length);
+                console.log(turnCount);
+             }
+        }
+        fillMoves(moves, turnCount)
+        console.log(moves)
+
+        const shining = (moves:string[]) => {
+            moves.map((move:string) => {
+                let shine:Boolean = true;
+                shine && document.getElementById(`${move}`)?.classList.add("opacity-50");
+                setTimeout(() => {shine = false; shine && document.getElementById(`${move}`)?.classList.remove("opacity-50")}, 100);
+            })
+        }
+
+        shining(moves);
         
     };
+
+    const stopGame = () => {
+        setGame({...game, playerTurn: false});
+    }
 
   return (
     <div className='mt-12 flex flex-col items-center'>
@@ -32,16 +56,16 @@ const Game = () => {
             {/* Game Plate */}
             <div className="flex flex-col gap-5">
                 <div className="flex flex-row gap-5">
-                    <div id='green' className="bg-greenCard w-32 h-32 cursor-pointer active:opacity-50"></div>
-                    <div id='red' className="bg-redCard w-32 h-32 cursor-pointer active:opacity-50"></div>
+                    <div id='green' className={` bg-greenCard w-32 h-32 cursor-pointer active:opacity-50`} ></div>
+                    <div id='red' className={` bg-redCard w-32 h-32 cursor-pointer active:opacity-50`}></div>
                 </div>
                 <div className="flex flex-row gap-5">
-                    <div id='yellow' className="bg-yellowCard w-32 h-32 cursor-pointer active:opacity-50"></div>
-                    <div id='blue' className="bg-blueCard w-32 h-32 cursor-pointer active:opacity-50"></div>
+                    <div id='yellow' className={` bg-yellowCard w-32 h-32 cursor-pointer active:opacity-50`}></div>
+                    <div id='blue' className={` bg-blueCard w-32 h-32 cursor-pointer active:opacity-50`}></div>
                 </div>
             </div>
             {/* Start Button */}
-            <button className='bg-cardDark w-[70%] self-center text-white text-3xl tracking-widest py-3 rounded-[10px] active:bg-white active:text-cardDark' onClick={startGame}>
+            <button className='bg-cardDark w-[70%] self-center text-white text-3xl tracking-widest py-3 rounded-[10px] active:bg-white active:text-cardDark' onClick={game.playerTurn ? stopGame : startGame}>
                 {game.playerTurn ? "STOP" : "START"}
             </button>
         </div>
