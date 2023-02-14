a415ff8ae33ce6613c32918915d1426336bd323f
TRMmember <gfakemail18@gmail.com>
2023-02-14 18:05:39 +0500

    finaly working shining

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,6 +3,10 @@ import {button} from '../constants/style'
 
 const Game = () => {
 
+    function timeout(ms:number) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
     const colors:string[] = ["green","red","yellow","blue"];
 
     type game = {
@@ -15,63 +19,47 @@ const Game = () => {
         score: 0,
     });
 
+    const moves:string[] = [];
 
-    let moves:string[] = [];
-    let turnCount = 1;
     const startGame = () => {
         setGame({...game, gameStarted: true});
-        movesFilling();
-        setTimeout(() => shining(moves), 1000);
-        console.log("shining done");
-        console.log(moves);
-        console.log(moves);
-        //checking time
 
+        console.log("Game started. Moves: " + moves);
+        fillMoves(moves);
+        console.log("moves were filled: " + moves);
+        shiningTurn(moves);
+        
     };
 
-    const movesFilling = () => {
-        const fillMoves = (moves:string[], turnCount:number) => {
-            while(moves.length < turnCount) {
-                console.log(moves.length);
-                console.log(turnCount);
-                moves.push(colors[Math.floor(Math.random()*colors.length)])
-                console.log(moves.length);
-                console.log(turnCount);
+    const fillMoves = (moves:string[]) => {
+        moves.push(colors[Math.floor(Math.random()*colors.length)]);
+        moves.push(colors[Math.floor(Math.random()*colors.length)]);
+    }
+
+
+    const shiningTurn = (moves:string[]) => {
+        for (let i = 0; i < moves.length; i++) {
+            setTimeout(function timer() {
+                document.getElementById(`${moves[i]}`)?.classList.add("opacity-50");
+                setTimeout(() => {document.getElementById(`${moves[i]}`)?.classList.remove("opacity-50")}, 500);
+            }, i * 1000);
              }
-        }
-        fillMoves(moves, turnCount);
-        console.log("moves were filled");
-
     }
 
-    const shining = (moves:string[]) => {
-        moves.map((move:string) => {
-            let shine:Boolean = true;
-            shine && document.getElementById(`${move}`)?.classList.add("opacity-60");
-            setTimeout(() => {shine = false; document.getElementById(`${move}`)?.classList.remove("opacity-60")}, 500);
-        })
-    }
-    shining(moves);
-
+  
 
     const checkingTime = (id:string) => {
-        let checked = turnCount;
-        while(checked != 0) {
-            if(id === moves[-1]) {
-                console.log("you're right for that push");
-                checked -= 1;
-                if (checked === 0) {
-                    break;
-                }
+        let gameLost = false;
+        moves.map((move) => {
+            if(id === move) {
+                console.log("That push was right!");
             } else {
-                console.log("you lose");
-                setGame({...game, gameStarted: false});
-                break;
+                console.log("You lose!");
+                stopGame();
+                gameLost = true;
             }
-        }
-        console.log("checking ends");
-        if(checked != 0) {setTimeout(() => movesFilling(), 1000);
-        shining(moves);}
+        })
+        !gameLost && startGame();
     }
 
     const stopGame = () => {
@@ -101,7 +89,7 @@ const Game = () => {
                 {game.gameStarted ? "STOP" : "START"}
             </button>
         </div>
-        {/* Game Block */}
+        {/* Game Block End*/}
     </div>
   )
 }
